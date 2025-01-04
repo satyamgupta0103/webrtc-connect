db0c6e9be006ec827647ab6611dc351ce8b2b547
Satyam <74068263+satyamgupta0103@users.noreply.github.com>
2025-01-04 23:58:09 +0530

    Updated backend server.js file

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,19 @@ const io = new Server(8000, {
   cors: true,
 });
 
+const nameToSocketIdMap = new Map();
+const socketIdToNameMap = new Map();
+
 io.on("connection", (socket) => {
   console.log(`Socket Connected`, socket.id);
 
   socket.on("room:join", (data) => {
-    console.log(data);
+    //console.log(data);
+    const { name, room } = data;
+    nameToSocketIdMap.set(name, socket.id);
+    socketIdToNameMap.set(socket.id, name);
+    io.to(room).emit("user:joined", { name, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
   });
 });
