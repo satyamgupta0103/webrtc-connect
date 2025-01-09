125c1311e34670b1a5e28efcaa3d7b8e8d726575
Satyam <74068263+satyamgupta0103@users.noreply.github.com>
2025-01-09 23:47:34 +0530

    App.jsx route added for Room

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 import Header from "./components/Header";
 import ParticipantsList from "./components/ParticipantsList";
+import VideoGrid from "./components/VideoGrid";
+import HomePage from "./components/HomePage";
+import { SocketProvider } from "./Context/SocketProvider";
+import RoomPage from "./components/Room";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header />
-      <ParticipantsList participants={["Satyam", "Sumit"]} />
+      <BrowserRouter>
+        <SocketProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/room/:roomId" element={<RoomPage />} />
+          </Routes>
+        </SocketProvider>
+      </BrowserRouter>
     </>
   );
 }
