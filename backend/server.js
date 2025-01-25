e50bd7cc746ccb014b4a5d6a01522d401838ffa6
Satyam <74068263+satyamgupta0103@users.noreply.github.com>
2025-01-25 15:55:19 +0530

    Implement logic in server.js to send the updated participant list to the client

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const io = new Server(8000, {
 
 const nameToSocketIdMap = new Map();
 const socketIdToNameMap = new Map();
+const roomParticipants = new Map(); // Map to store participants for each room
 
 io.on("connection", (socket) => {
   console.log(`Socket Connected`, socket.id);
@@ -15,6 +16,27 @@ io.on("connection", (socket) => {
     const { name, room } = data;
     nameToSocketIdMap.set(name, socket.id);
     socketIdToNameMap.set(socket.id, name);
+
+    // Manage participants in the room
+    if (!roomParticipants.has(room)) {
+      roomParticipants.set(room, new Map());
+    }
+    roomParticipants.get(room).set(socket.id, name);
+    //console.log(roomParticipants);
+
+    //const serializedParticipantsMap = [...roomParticipants.entries()];
+
+    const serializedParticipantsArray = Array.from(
+      roomParticipants.values()
+    ).flatMap((participantsMap) =>
+      Array.from(participantsMap.entries()).map(([id, name]) => ({ id, name }))
+    );
+
+    console.log(serializedParticipantsArray);
+
+    // Notify everyone in the room about the new participant
+    io.to(room).emit("room:participants", serializedParticipantsArray);
+
     io.to(room).emit("user:joined", { name, id: socket.id });
     socket.join(room);
     io.to(socket.id).emit("room:join", data);
