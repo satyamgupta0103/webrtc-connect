891628e1e1115bc6bf9363df8954c298cb7228d0
Satyam <74068263+satyamgupta0103@users.noreply.github.com>
2025-02-03 00:00:09 +0530

    ParticipantLisy.jsx: Updated

--- src/components/ParticipantsList.jsx
+++ src/components/ParticipantsList.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function ParticipantsList({ participants }) {
   return (
     <>
-      <aside className="w-1/4 bg-gray-800 text-white p-4">
+      <aside className="h-screen w-1/4 bg-gray-800 text-white p-4 ">
         <h2 className="text-lg font-bold mb-4">
           Participants <span>{participants.length}</span>
         </h2>
         <ul>
           {participants.map((participant) => {
             return (
-              <li className="flex items-center mb-2">
-                <span className="bg-green-500 h-3 w-3 rounded-full mr-2"></span>
-                {participant}
+              <li className="flex items-center mb-2" key={participant.id}>
+                <span className="bg-green-500 h-2 w-2 rounded-full mr-2"></span>
+                {participant.name}
               </li>
             );
           })}
