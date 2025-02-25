91d2a7b0578d8279899677ee86b931e3f6424818
Satyam <74068263+satyamgupta0103@users.noreply.github.com>
2025-02-25 23:41:19 +0530

    Updated callpage design: CallPageHeader, CallPageFooter, MeetingInfo Components added, styles and layout improved

--- client/src/components/CallPage/CallPage.jsx
+++ client/src/components/CallPage/CallPage.jsx
@@ -1,9 +1,17 @@
+import CallPageFooter from "../UI/CallPageFooter/CallPageFooter";
+import CallPageHeader from "../UI/CallPageHeader/CallPageHeader";
+import MeetingInfo from "../UI/MeetingInfo/MeetingInfo";
+import Messenger from "../UI/Messenger/Messenger";
+
 import "./CallPage.scss";
 
 const CallPage = () => {
   return (
-    <div>
-      <h1>CallPage</h1>
+    <div className="callpage-container">
+      <video className="video-container" src="" controls></video>
+      <CallPageHeader />
+      <CallPageFooter />
+      <MeetingInfo />
     </div>
   );
 };
