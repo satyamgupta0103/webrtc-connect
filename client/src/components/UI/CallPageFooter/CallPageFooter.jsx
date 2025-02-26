a7643b4c9fdd2b05ee6dcc5a38eabf0a7ae4d836
Satyam <74068263+satyamgupta0103@users.noreply.github.com>
2025-02-26 23:14:05 +0530

    Enhanced UI/UX for CallPageHeader, CallPageFooter, MeetingInfo, and Messenger components with improved layout and styling.

---
+++ client/src/components/UI/CallPageFooter/CallPageFooter.jsx
@@ -0,0 +1,46 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./CallPageFooter.scss";
+import {
+  faAngleUp,
+  faClosedCaptioning,
+  faDesktop,
+  faMicrophone,
+  faPhone,
+  faVideo,
+} from "@fortawesome/free-solid-svg-icons";
+
+const CallPageFooter = () => {
+  return (
+    <div className="footer-item">
+      <div className="left-item">
+        <div className="icon-block">
+          Meeting details
+          <FontAwesomeIcon className="icon" icon={faAngleUp} />
+        </div>
+      </div>
+      <div className="center-item">
+        <div className="icon-block">
+          <FontAwesomeIcon className="icon" icon={faMicrophone} />
+        </div>
+        <div className="icon-block">
+          <FontAwesomeIcon className="icon red" icon={faPhone} />
+        </div>
+        <div className="icon-block">
+          <FontAwesomeIcon className="icon" icon={faVideo} />
+        </div>
+      </div>
+      <div className="right-item">
+        <div className="icon-block">
+          <FontAwesomeIcon className="icon red" icon={faClosedCaptioning} />
+          <p className="title">Turn on captions</p>
+        </div>
+        <div className="icon-block">
+          <FontAwesomeIcon className="icon red" icon={faDesktop} />
+          <p className="title">Present now</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CallPageFooter;
