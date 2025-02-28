99e3853a601db4e369cc58a880267910ea278565
Satyam <74068263+satyamgupta0103@users.noreply.github.com>
2025-02-28 23:55:24 +0530

    NoMatch: NoMatch component UI and Layout updated

--- client/src/components/NoMatch/NoMatch.jsx
+++ client/src/components/NoMatch/NoMatch.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
 import "./NoMatch.scss";
+import Header from "../UI/Header/Header";
 
 const NoMatch = () => {
   return (
-    <div>
-      <h1>NoMatch</h1>
+    <div className="no-match">
+      <Header />
+      <div className="no-match-content">
+        <h2>Invalid video call link.</h2>
+        <div className="action-btn">
+          <Link className="btn green" to="/">
+            Return to home screen
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
